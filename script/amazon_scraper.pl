ab010008c126d7ee69e36a1cace1cb053b64436d
Luis Vitor <luis.vitor@dcx.ufpb.br>
2021-09-01 20:14:38 -0300

    script para deletar linhas com valores nulos

--- script/amazon_scraper.pl
+++ script/amazon_scraper.pl
@@ -11,7 +11,7 @@ my $dbh = DBI -> connect("DBI:$driver:dbname=$path_db","","", { RaiseError => 1 
 
 print "DataBase Opened";
 
-=a
+# script for create table
 my $table_create = qq( CREATE TABLE SCRAPER
                         (
                         PRODUCT_NAME TEXT,
@@ -27,18 +27,16 @@ if($migration < 0){
 }else{
     print "Table Created";
 }
-=cut
 my $authors = scraper {
 
-    process 'div[class="sg-col-inner"] div[class="a-section a-spacing-none"]', "prices[]" => scraper {
+    process 'div[class="sg-row"]', "prices[]" => scraper {
 
-      process 'span[class="a-size-medium a-color-base a-text-normal"]', product_name => 'TEXT';
-      process 'span[class="a-price-whole"]', price => 'TEXT';
-
+      process 'h2', product_name => 'TEXT';
+      process 'span[class="a-price"] span[class="a-offscreen"]', price => 'TEXT';
     };
 };
  
-my $res = $authors->scrape( URI->new("https://www.amazon.com/s?k=mouse") );
+my $res = $authors->scrape( URI->new("https://www.amazon.com/s?k=mouse&__mk_pt_BR=ÅMÅŽÕÑ&ref=nb_sb_noss_2") );
 
 for my $author (@{$res->{prices}}) {
     my $insert = qq(
@@ -47,11 +45,16 @@ for my $author (@{$res->{prices}}) {
             "$author->{product_name}",
             "$author->{price}")        
     );
-    print Encode::encode("utf8", "$author->{product_name}\t$author->{price}\n");
+    print Encode::encode("utf8", "$author->{price}\n");
+    print Encode::encode("utf8", "$author->{product_name}\n");
 
     my $exec = $dbh -> do($insert) or die $DBI::errstr;
 }
 
+#script para deletar linhas nulas
+my $delete_null = qq( DELETE FROM SCRAPER WHERE PRODUCT_NAME="" OR PRODUCT_NAME IS NULL OR PRICE="" OR PRICE IS NULL);
+
+my $exec_delete = $dbh -> do($delete_null) or die $DBI::errstr;
 
 
 $dbh->disconnect();
